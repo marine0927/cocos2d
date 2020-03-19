10222c4674b6e23bd3b4172779ed79a6cd146318
marine0927 <45658966+marine0927@users.noreply.github.com>
2020-03-20 02:29:39 +0800

    Add files via upload

---
+++ Resources/32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="32x32" tilewidth="32" tileheight="32" tilecount="4544" columns="64">
+ <image source="32x32.bmp" width="2048" height="2272"/>
+</tileset>
