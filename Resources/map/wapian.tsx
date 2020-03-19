bc900f92de897b5291fbee8ad3b79dd6a70f4511
marine0927 <45658966+marine0927@users.noreply.github.com>
2020-03-20 02:16:52 +0800

    Add files via upload

---
+++ Resources/map/wapian.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="wapian" tilewidth="60" tileheight="60" tilecount="49" columns="7">
+ <image source="wapian.png" width="458" height="442"/>
+</tileset>
---
+++ Resources/map/wapian2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="wapian2" tilewidth="64" tileheight="64" tilecount="100" columns="10">
+ <image source="wapian2.png" width="640" height="640"/>
+</tileset>
